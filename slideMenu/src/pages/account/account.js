8f37f4f98d6344e5e93dca0ec5ab518cfacf4ed2
lchgl <403552501@qq.com>
2018-06-21 10:20:27 +0800

    回滚1

--- slideMenu/src/pages/account/account.js
+++ slideMenu/src/pages/account/account.js
@@ -8,90 +8,33 @@ import {
     View,
     Text,
     Button,
-    Platform,
-	Image
+    Platform
 } from 'react-native';
 
 type
 Props = {};
 
-let REQUEST_URL = 'https://raw.githubusercontent.com/facebook/react-native/master/docs/MoviesExample.json';
 
 export default class account extends Component<Props> {
     static navigationOption = {
         title: '账户'
     };
-	constructor(props){
-		super(props);
-		this.state = {
-			movies:null,
-		}
-	}
     render() {
-        if(!this.state.movies){
-			return this.rederLoadingView();
-		}
-		let movie = this.state.movies[0];
-		return this.rederMovies(movie);
+        const {navigation} = this.props;
+        return (
+            <View style={styles.container}>
+                <Text>欢迎来到账户主页</Text>
+                <Button title="返回" onPress={()=>navigation.goBack()}/>
+            </View>
+        )
     }
-	fetchData(){
-		fetch(REQUEST_URL,{
-			method:'GET'
-		}).then((responseData)=>{
-			this.setState({
-				movies:resposeData.movies,
-			})
-			console.log(responseData)
-		}).catch((error)=>{
-			console.log(error)
-		})
-	}
-	componentDidMount(){
-		this.fetchData();
-	}
-	rederLoadingView(){
-		return (
-			<View style={styles.container}> 
-				<Text>
-					正在加载电影数据···
-				</Text>
-			</View>
-		)
-	}
-	rederMovies(movie){
-		return (
-			<View style={styles.container}>
-					<Image source={{uri:movie.posters.thumbnail}} style={styles.thumbnail} />
-					<View style={styles.rightContainer}>
-						<Text style={styles.title}>{movie.title}</Text>
-						<Text style={styles.year}>{movie.year}</Text>
-					</View>
-			</View>
-		)
-	}
 }
 
 const styles = StyleSheet.create({
     container: {
         flex:1,
-		flexDirection:'row',
         justifyContent:'center',
         alignItems:'center',
         backgroundColor:'#eeeeee'
-    },
-	thumbnail:{
-		width:100,
-		height:80
-	},
-	rightContainer:{
-		flex:1
-	},
-	title:{
-		fontSize:20,
-		marginBottom:8,
-		textAlign:'center'
-	},
-	year:{
-		textAlign:'center'
-	}
+    }
 });
